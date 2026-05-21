c55d8106becbc52b116ed244d1ff7b5de431b59b
lucaspielt23-sys <lucaspielt23@gmail.com>
2026-05-21 16:26:28 +0200

    Add files via upload

---
+++ src/main/java/de/sadserver/SadServerPlugin.java
@@ -0,0 +1,12 @@
+
+package de.sadserver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SadServerPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("SadServerPlugin aktiviert!");
+    }
+}
